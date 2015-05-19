d154373d82ec593325f86019f782b7a2198c02ba
nickpwells <wells.nick53@gmail.com>
2015-05-18 20:17:43 -0700

    working fizzbuzz refractor

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+	function fizzbuzz(max) {
+		var count = 1;
+		for (var i = 1; i <= max; i++) {
+			if (i % 5 == 0 && i % 3 == 0) {
+      			$("#console").append("<p>" + count + ": " + "FizzBuzz" + "</p>");
+      			count += 1;
+    		} 
+    		else if ( i % 3 == 0 ) {
+      			$("#console").append("<p>" + count + ": " + "Fizz" + "</p>");
+      			count += 1;
+    		} 
+    		else if (i % 5 == 0) {
+      			$("#console").append("<p>" + count + ": " + "Buzz" + "</p>");
+      			count += 1;
+    		} 
+    		else {
+      			$("#console").append("<p>" + count + ": " + i + "</p>");
+      			count += 1;
+    		}
+		}
+	}
+
+	function runFizzbuzz() {
+		$('input').keydown(function(enter){
+			if (enter.keyCode == 13) {
+				enter.preventDefault();
+				$('p').remove();
+				var userInput = $(this).val();
+				var stringNum = parseInt(userInput, 10);
+				var num = + stringNum;
+				fizzbuzz(num);
+			}
+		})
+	}
+
+	runFizzbuzz();
+
+});
